610791fa032f1431e8f68ef02f271d15139bf89c
Shubh Sharma <147989825+ShubhSharma34@users.noreply.github.com>
2024-02-27 18:52:58 +0530

    Ques1(b)Feb21.c

---
+++ Ques1(b)Feb21.c
@@ -0,0 +1,14 @@
+21Feb2024
+  //Code to find length of the string With using Linraray functions
+#include<stdio.h>
+  #include<string.h>
+  int main()
+{
+  int l;
+char s[20];
+printf("Enter the charcters of the string\n");
+fgets(s,20,stdin);
+l=strlen(s);
+printf("%d is the length of length of the string\n");
+return 0;
+}
